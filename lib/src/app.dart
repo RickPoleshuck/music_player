f48e312fbf9348c89486f3f5cd26e06af21ce3f1
rick <RickPoleshuck@gmail.com>
2023-10-13 10:25:03 -0400

    junk

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'sample_feature/sample_item_details_view.dart';
 import 'home/home_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
@@ -45,8 +43,6 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
                   case HomeView.routeName:
                   default:
                     return const HomeView();
